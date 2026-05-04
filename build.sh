679f20272ed7403af76e3eb9ce5670c24e05ef7c
zbxhhhhhh <zbxhhhhhh@gmail.com>
2026-05-04 20:01:53 +0300

    Add Railway deployment files to root directory

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit
+
+cd backend
+
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py seed_super_employee
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd backend
+gunicorn university_activities.wsgi:application --bind 0.0.0.0:$PORT
